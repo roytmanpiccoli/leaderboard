d1e0757028a867e1c3d4e8861d788fc4a7da14c2
Roytman Piccoli <roytmanpiccoli@macbook-air-de-roytman.home>
2021-09-06 09:24:05 +0200

    Initial commit

--- env.sh
+++
@@ -1,4 +0,0 @@
-export AWS_REGION=us-east-1
-export DATABASE_ARN=arn:aws:rds:us-east-1:119146392839:cluster:leaderboard
-export SECRET_ARN=arn:aws:secretsmanager:us-east-1:119146392839:secret:leaderboard-database1-1SIY7m
-export SECURITY_GROUP_ID=sg-0570fe492db9d4fad
